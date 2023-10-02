9a9b5838da6259c86c5505f84c76692f81a7bb9b
David Legendre <legendre2013@hotmail.com>
2023-10-02 18:30:02 -0500

    emit

--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,7 @@ import { cookies } from 'next/headers'
 import '../components/css/Home.class.css'
 import AboutMeSection from "@/components/SectionAboutMe"
 import ProjectSection from "@/components/ProjectsSection"
-import { Root } from '../Interfaces/Portfolio.interface';
-
+import { ToastContainer } from "react-toastify"
 
 export default async function(){
     const supabase = createServerComponentClient({cookies})
@@ -12,11 +11,13 @@ export default async function(){
     const { data : Root} = await supabase
     .from('Porfolio')
     .select('*, Captures(*), Links(*, TypeURLs(*))')
-
     
     return (
         <>
         <AboutMeSection></AboutMeSection>
+        <ToastContainer
+      />
+      <ToastContainer />
         <div className="homePage">
             <p className="title-projects">Proyecto Realizados</p>
         { !Root ? 
